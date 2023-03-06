f64c8848889b8f288d254a0f77daa01179904e20
Mouad <mouad10wolf@gmail.com>
2023-03-06 17:46:36 +0100

    Adding Views

--- profile_project/api/views.py
+++ profile_project/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from profile_project.api.serializers import UserSerializer, GroupSerializer
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
