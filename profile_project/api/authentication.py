23a3ba053a2f9c2f5d71c50bf71caaee90057a93
Mouad <mouad10wolf@gmail.com>
2023-03-08 00:44:04 +0100

    Add Authentication middleware

---
+++ profile_project/api/authentication.py
@@ -0,0 +1,39 @@
+# accounts.authentication
+
+import jwt
+from rest_framework.authentication import BaseAuthentication
+from django.middleware.csrf import CsrfViewMiddleware
+from rest_framework import exceptions
+from django.conf import settings
+from django.contrib.auth import get_user_model
+import requests
+
+
+class SafeJWTAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+
+        User = get_user_model()
+        authorization_heaader = request.headers.get('Authorization')
+
+        if not authorization_heaader:
+            return None
+        try:
+            # header = 'Token xxxxxxxxxxxxxxxxxxxxxxxx'
+            access_token = authorization_heaader.split(' ')[1]
+
+            response = requests.get(settings.AUTH_ENDPOINT, headers={'Authorization': authorization_heaader}).json()
+
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('access_token expired')
+        except IndexError:
+            raise exceptions.AuthenticationFailed('Token prefix missing')
+
+        user = User.objects.filter(id=response['user_id']).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed('user is inactive')
+
+        return (user, None)
