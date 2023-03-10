99edaff70ca7de97d4e9fb9c50b98d83237b8d7b
Mouad <mouad10wolf@gmail.com>
2023-03-10 11:15:32 +0100

    Add profile model

--- profile_project/api/models.py
+++ profile_project/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+class Profile(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(unique=True, max_length=255)
+    name = models.CharField(max_length=255)
+    birth_date = models.CharField(max_length=255)
+    bio = models.TextField()
+    website = models.CharField(max_length=255)
+    avatar = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+        def __unicode__(self):
+            return self.username
