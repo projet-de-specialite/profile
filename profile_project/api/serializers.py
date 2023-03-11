eb0d93a82f06d8e1c6762546a41b5edd84638a88
Mouad <mouad10wolf@gmail.com>
2023-03-11 11:23:02 +0100

    Add ProfileSerializer

--- profile_project/api/serializers.py
+++ profile_project/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from profile_project.api.models import Profile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'birth_date', 'created_on', 'id', 'name', 'user_id', 'username', 'website']
