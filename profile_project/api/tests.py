b025ebfde078cde09694e0eda0eb0c26e90e3fb3
Mouad <mouad10wolf@gmail.com>
2023-03-15 11:55:36 +0100

    Add first profile test

--- profile_project/api/tests.py
+++ profile_project/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+# Using the standard RequestFactory API to create a form POST request
+factory = APIRequestFactory()
+request = factory.post('/profiles/',
+    {
+    "avatar": "https://pedag.u-picardie.fr/moodle/upjv/pluginfile.php/1/core_admin/logocompact/300x300/1677060056/Logo_UPJV_navbar.png",
+    "bio": "Hello",
+    "birth_date": "01/02/1997",
+    "created_on": "2023-03-15T07:08:43.233477Z",
+    "id": 1,
+    "name": "Mouad",
+    "user_id": 1,
+    "username": "mouaddb",
+    "website": "mouad.ma"
+    }
+)
+
